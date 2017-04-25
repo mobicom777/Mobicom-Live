e5fd0193a501152a4483ff556d8b88fb37396dd2
mobicom777 <j.mobicom@gmail.com>
2017-04-25 14:05:54 +1000

    Update main.js

--- main.js
+++ main.js
@@ -15,5 +15,5 @@
 myPlayer = amp("azuremediaplayer", myOptions);
 
 myPlayer.src([
-    { src: "http://mobicom.streaming.mediaservices.windows.net/3d30369e-0658-4ff5-a79c-3659cde656a1/Kal Ho Naa Ho - Title Track Video  Shahrukh Khan, Saif, Preity.ism/manifest", type: "application/vnd.ms-sstr+xml", streamingFormats: ["SMOOTH", "HLS","HLS-V3", "HLS-V4"]}, 
+    { src: "http://mobicom.streaming.mediaservices.windows.net/8d842291-3599-4d36-87fc-f2f34ece2d4e/d6cc5050-3613-46b9-a98a-46ea91cda061.ism/manifest", type: "application/vnd.ms-sstr+xml", streamingFormats: ["SMOOTH", "HLS","HLS-V3", "HLS-V4"]}, 
 ]);
